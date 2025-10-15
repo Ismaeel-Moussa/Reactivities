b9d618808395638f79889741c15b2d8e615f018d
Ismaeel <ismaeel.moussa1@gmail.com>
2025-10-15 17:24:47 +0300

    Create initial .NET backend structure and database

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,37 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController(AppDbContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(string id)
+        {
+            var activity = await context.Activities.FindAsync(id);
+
+            if (activity == null) return NotFound();
+
+            return activity;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            context.Activities.Add(activity);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetActivity), new { id = activity.Id }, activity);
+        }
+
+        
+
+    }
+}
